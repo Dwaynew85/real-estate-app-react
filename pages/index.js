d38ff944143d02ae2442a0faf4d85200487cb34b
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-12-08 02:46:56 -0500

    changed div to Box and added API call logic to props

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 import {Flex, Box, Text, Button} from '@chakra-ui/react';
 
+import { baseUrl, fetchApi } from "../utils/fetchApi";
+
 const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
     <Image src={imageUrl} width={500} height={300} alt="banner" />
@@ -16,9 +18,9 @@ const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, i
   </Flex>
 )
 
-export default function Home() {
+export default function Home({ propertiesForSale, propertiesForRent }) {
   return (
-    <div>
+    <Box>
       <h1>Hello World</h1>
       <Banner 
         purpose={'RENT A HOME'} 
@@ -30,6 +32,9 @@ export default function Home() {
         linkName="/search?purpose=for-rent"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"  
       />
+      <Flex flexWrap="wrap">
+        {/* Fetch the properties and map over them... */}
+      </Flex>
       <Banner 
         purpose={'BUY A HOME'} 
         title1="Find, Buy & Own Your"
@@ -40,6 +45,21 @@ export default function Home() {
         linkName="/search?purpose=for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"  
       />
-    </div>
+        {/* Fetch the properties for buying ahome... */}
+
+    </Box>
   )
+}
+
+
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`)
+  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`)
+
+  return {
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+    }
+  }
 }
