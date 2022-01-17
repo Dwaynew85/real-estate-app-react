bafa05002e9fa615297d32a3a2c0c222bf1fb714
Dwaynew85 <mr_dwayne_w@yahoo.com>
2022-01-17 00:22:11 -0500

    refactored and removed banner compnent logic

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,7 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import {Flex, Box, Text, Button} from '@chakra-ui/react';
-
+import {Flex, Box } from '@chakra-ui/react';
 import { baseUrl, fetchApi } from '../utils/fetchApi';
 
-const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
-  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
-    <Image src={imageUrl} width={500} height={300} alt="banner" />
-    <Box p="5">
-      <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
-      <Text fontSize="3xl" fontWeight="bold">{title1}<br />{title2}</Text>
-      <Text fontSize="lg" paddingTop="3" paddingBottom="3" color="gray.700">{desc1}<br />{desc2}</Text>
-      <Button fontSize="xl">
-        <Link href={linkName}>{buttonText}</Link>
-      </Button>
-    </Box>
-  </Flex>
-)
+import Banner from "../components/Banner"
 
 export default function Home({ propertiesForSale, propertiesForRent }) {
   console.log(propertiesForSale, propertiesForRent);
@@ -46,7 +31,7 @@ export default function Home({ propertiesForSale, propertiesForRent }) {
         linkName="/search?purpose=for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"  
       />
-        {/* Fetch the properties for buying ahome... */}
+        {/* Fetch the properties for buying a home... */}
 
     </Box>
   )
